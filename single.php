0392da65a198932f35a908cf0604a05c658e71a4
Carlos Coello <carlos.coello05@outlook.com>
2017-03-08 00:23:10 -0600

    single file

---
+++ single.php
@@ -0,0 +1,43 @@
+<?php /*
+
+@package sunsettheme
+*/
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+
+
+						<?php
+
+							if( have_posts() ):
+
+								while( have_posts() ): the_post();
+
+
+
+									get_template_part( 'template-parts/single', get_post_format() );?>
+
+									<div class="container">
+								<?php
+
+									if ( comments_open() ):
+										comments_template(); ?>
+									</div>
+
+								<?php	endif;
+
+								endwhile;
+
+							endif;
+
+						?>
+
+
+
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>

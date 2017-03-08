4abaa6a3a65bd7aea4078bc2928d2bf3f52e96e1
Carlos Coello <carlos.coello05@outlook.com>
2017-03-08 00:06:45 -0600

    comments file

---
+++ comments.php
@@ -0,0 +1,56 @@
+<?php
+/*
+
+Comments
+
+*/
+
+if( post_password_required() ){
+
+  return;
+
+}
+
+
+?>
+
+<div id="comments" class="comments-area">
+
+<?php if( have_comments() ){ ;?>
+  <h2>
+    <?php printf(
+      esc_html( _nx( 'One comment on &ldquo;%2$s&rdquo;', '%1$s comments on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'thirty-eight' ) ),
+      number_format_i18n( get_comments_number() ),
+      '<span>' . get_the_title() . '</span>'
+    ); ?>
+  </h2>
+  <ol class="comment-list">
+    <?php
+    $args = array(
+      'walker'            => null,
+      'max_depth'         => '',
+      'style'             => 'ol',
+      'callback'          => null,
+      'end-callback'      => null,
+      'type'              => 'all',
+      'reply_text'        => 'Reply',
+      'page'              => '',
+      'per_page'          => '',
+      'avatar'            => 32,
+      'reverse_top_level' => true,
+      'reverse_childrend'   => true,
+      'format'            => 'html5',
+      'short_ping'        => false,
+      'echo'              => true
+
+    );
+    wp_list_comments( $args ); ?>
+  </ol>
+    <?php if( !comments_open() && get_comments_number() ){;?>
+        <p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'thirty-eight' ); ?></p>
+    <?php };?>
+<?php }; ?>
+
+<?php comment_form(  ); ?>
+
+</div><!-- .comments-area -->

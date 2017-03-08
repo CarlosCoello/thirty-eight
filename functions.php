3481199592dadec2b87d6e5f64f5a61e76c4475f
Carlos Coello <carlos.coello05@outlook.com>
2017-03-08 00:08:21 -0600

    functions file

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+
+ require get_template_directory() . '/inc/enqueue.php';
+ require get_template_directory() . '/inc/function-admin.php';
+ require WP_PLUGIN_DIR . '/nnscrolltop/inc/function-admin-plugin.php';
+ require get_template_directory() . '/inc/theme-support.php';
+ require get_template_directory() . '/inc/wp_bootstrap_navwalker.php';
+ require get_template_directory() . '/inc/ajax.php';
+ require get_template_directory() . '/inc/activate-contact-form.php';
+ require get_template_directory() . '/inc/widgets.php';
+
+?>
